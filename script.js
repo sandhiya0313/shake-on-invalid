9ea51d8471375dceb3404389c764a3919002f1ee
sandhiya0313 <sandhiyagovindaraj410@gmail.com>
2024-08-13 20:20:47 +0530

    new

---
+++ script.js
@@ -0,0 +1,21 @@
+function validateInput() {
+    let inputField = document.getElementById("inputField");
+    let inputValue = inputField.value.trim();
+    let errorMessage = document.getElementById("errorMessage");
+  
+    //Add your validation logic here
+    if (inputValue === "") {
+      //Invalud input,apply shake effect and display the error message
+      inputField.classList.add("shake");
+      errorMessage.style.visibility = "visible";
+  
+      //Remove the shake class and hide the error message after the animation completes
+      setTimeout(function () {
+        inputField.classList.remove("shake");
+        errorMessage.style.visibility = "hidden";
+      }, 500);
+    } else {
+      //Valid input, proceed with your logic
+      alert("Valid input");
+    }
+  }
